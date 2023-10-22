ea4afb0748d9d299e9910e00815b447179ae8c9d
Sanglafort <silviaangfort@gmail.com>
2023-10-22 19:00:58 +0200

    Finalizo bloque 1.4

--- Bloque 1.4. Rest & Spread operators/index.js
+++ Bloque 1.4. Rest & Spread operators/index.js
@@ -26,6 +26,48 @@ Copiant objectes amb Spread: Crea un objecte 'objecte1'. Després crea un segon 
 
 const objecte1 = {
     nom: "Rosa",
-    cognom: "Peral"
+    cognom: "Peral",
+}
+console.log("Ejercicio 3:\n (en objeto2 se añade la edad)")
+const objecte2 = {...objecte1, age:42};
+console.log(objecte2, objecte1)
+
+/* Exercici 4
+Rest en Destructuring: Crea una array amb diversos elements. Utilitza destructuring i l'operador rest per a assignar els primers dos elements a variables, i després assignar la resta dels elements a una tercera variable. */
+
+const array = [45, 36, 98, 0, 15];
+console.log("Ejercicio 4:\n Array = [45, 36, 98, 0, 15]");
+
+const [primero, segundo, ...resto] = array;
+console.log("  Primer elemento: " + primero);
+console.log("  Segundo elemento: " + segundo);
+console.log("  Y el resto: " + resto.join(", "));
+
+/* Exercici 5
+Spread en Funcions: Crea una funció que accepti tres arguments. Després, crea una array amb tres elements i crida la funció utilitzant l'operador spread amb aquesta array. */
+
+function add(a, b, c) {
+    return a + b + c;
 }
 
+const numbers = [2, 5, 8];
+
+console.log("Ejercicio 5:\n La suma de 2, 5 y 8 es: " + add(...numbers));
+
+/* Exercici 6
+Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou. */
+
+const car = {
+    brand: "Ford",
+    model: "Fiesta",
+    color: "red"
+}
+
+const home = {
+    tipus: "Masia",
+    floors: 3
+}
+
+const myProperties = {...car, ...home};
+
+console.log("Ejercicio 6:\n" + JSON.stringify(myProperties));
